916940f743557fd5e61a133cda4a75727b636104
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-05 18:36:42 -0700

    add code to redirect after form submission

--- express_server.js
+++ express_server.js
@@ -42,8 +42,11 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  const templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
