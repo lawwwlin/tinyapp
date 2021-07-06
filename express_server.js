d4b7e9784acceac135defd9881333fea9afc2c7e
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-05 20:18:12 -0700

    update wrong if statement in checking if url exist

--- express_server.js
+++ express_server.js
@@ -41,7 +41,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  if () {
+  if (urlDatabase[req.params.shortURL]) {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   } else {
