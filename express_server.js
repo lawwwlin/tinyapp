4040134cf54c4eb22ea0f97f65b04d3cf5d7856d
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-07 14:54:14 -0700

    add new register page with account creation fields

--- express_server.js
+++ express_server.js
@@ -107,6 +107,13 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    };
+  res.render("urls_register", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
