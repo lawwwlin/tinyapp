5af10ec2181fd2aae99febeec093097908ed4542
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-08 16:24:48 -0700

    add 2 more tests for findUserByEmail

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -15,6 +15,8 @@ const testUsers = {
   }
 };
 
+const testUsers2 = {};
+
 describe('getUserByEmail', function() {
   it('should return a user object with matching email, given an email in the database', function() {
     const user = findUserByEmail("user@example.com", testUsers)
@@ -24,6 +26,18 @@ describe('getUserByEmail', function() {
 
   it('should return null, given an email not in the database', function() {
     const user = findUserByEmail("none@example.com", testUsers)
+    const expectedOutput = null;
+    assert.deepEqual(user, expectedOutput);
+  });
+  
+    it('should return null, given an empty string in the database', function() {
+      const user = findUserByEmail("", testUsers)
+      const expectedOutput = null;
+      assert.deepEqual(user, expectedOutput);
+    });
+
+  it('should return null, given an email not in and empty database', function() {
+    const user = findUserByEmail("user@example.com", testUsers2)
     const expectedOutput = null;
     assert.deepEqual(user, expectedOutput);
   });
